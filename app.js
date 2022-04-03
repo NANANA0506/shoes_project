95cd1a74862411414006089b0e058ae6967b7e0f
ODG <ohdonggeon3132@gmail.com>
2022-04-03 18:28:53 +0900

    main.pug test

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const globalRouter = require("./routers/globalRoter");
+
+const PORT = 3050;
+const app = express();
+
+app.set("view engine", "pug");
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.use("/", globalRouter);
+
+app.listen(PORT, () => {
+  console.log(`⭐️👟https://localhost:${PORT} ✨SERVER START💻🍀`);
+});
