d87cbb8c3ea84510b9e5be4dd109c9602a9ae84a
ODG <ohdonggeon3132@gmail.com>
2022-04-08 17:58:01 +0900

    siginin

--- routers/globalRoter.js
+++ routers/globalRoter.js
@@ -4,7 +4,10 @@ const checkLogin = require("../middlewares/checkLogin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("main");
+  const loggedIn = req.session.isLoggedIn;
+
+  console.log(req.session);
+  res.render("main", { loggedIn });
 });
 
 router.get("/product", (req, res, next) => {
@@ -13,11 +16,40 @@ router.get("/product", (req, res, next) => {
 
 router.get("/signin", checkLogin, (req, res, next) => {
   const loggedIn = req.session.isLoggedIn;
-  res.render("screens/signin", { loggedIn });
+  res.render("signin", { loggedIn });
 });
 
 router.post("/signin", (req, res) => {
   const selectQuery = `
+    SELECT  id,
+            name,
+            email,
+            password,
+            birth,
+            phone_number
+      FROM  users
+     WHERE  email = "${req.body.signinEmail}"
+       AND  password = "${req.body.signinPassword}"
   `;
+  try {
+    db.query(selectQuery, (error, rows) => {
+      if (error) {
+        console.error(error);
+        return res.redirect("/signin");
+      }
+
+      if (rows.length === 0) {
+        return res.redirect("/signin");
+      }
+      req.session.isLoggedIn = true;
+      req.session.userId = rows[0].id;
+      return res.redirect("/");
+    });
+  } catch (error) {
+    console.log(error);
+    return res.redirect("/signin");
+  }
 });
+
+router.get("/signup");
 module.exports = router;
